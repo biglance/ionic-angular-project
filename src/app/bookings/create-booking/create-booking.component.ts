6937b658e0c86fd52c1cd962a6a52e8ed7e64583
BSOLI <bright.soli@computacenter.com>
2023-08-10 13:25:04 +0200

    Offer form

--- src/app/bookings/create-booking/create-booking.component.ts
+++ src/app/bookings/create-booking/create-booking.component.ts
@@ -1,24 +1,83 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input, OnInit, ViewChild } from '@angular/core';
 import { Place } from '../../places/place.model';
 import { ModalController } from '@ionic/angular';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-create-booking',
   templateUrl: './create-booking.component.html',
   styleUrls: ['./create-booking.component.scss'],
 })
-export class CreateBookingComponent  implements OnInit {
+export class CreateBookingComponent implements OnInit {
   @Input() selectedPlace: Place;
+  @Input() selectedMode: 'select' | 'random';
+  @ViewChild('f', { static: true }) form: NgForm;
+  startDate: string;
+  formattedStartDate: string;
+  endDate: string;
+  formattedEndDate: string;
 
-  constructor(private modalCtrl: ModalController) { }
+  constructor(private modalCtrl: ModalController) {}
 
-  ngOnInit() {}
+  ngOnInit() {
+    const availableFrom = new Date(this.selectedPlace.availableFrom);
+    const availableTo = new Date(this.selectedPlace.availableTo);
+    if (this.selectedMode === 'random') {
+      this.startDate = new Date(
+        availableFrom.getTime() +
+          Math.random() *
+            (availableTo.getTime() -
+              7 * 24 * 60 * 60 * 1000 -
+              availableFrom.getTime())
+      ).toISOString();
+      this.endDate = new Date(
+        new Date(this.startDate).getTime() +
+          Math.random() *
+            (new Date(this.startDate).getTime() +
+              6 * 24 * 60 * 60 * 1000 -
+              new Date(this.startDate).getTime())
+      ).toISOString();
+
+      // Get the year, month, and day from the Date object
+      const year = new Date(this.startDate).getFullYear();
+      const month = String(new Date(this.startDate).getMonth() + 1).padStart(
+        2,
+        '0'
+      ); // Months are 0-based, so add 1
+      const day = String(new Date(this.startDate).getDate()).padStart(2, '0');
+
+      const yearEnd = new Date(this.endDate).getFullYear();
+      const monthEnd = String(new Date(this.endDate).getMonth() + 1).padStart(
+        2,
+        '0'
+      ); // Months are 0-based, so add 1
+      const dayEnd = String(new Date(this.endDate).getDate()).padStart(2, '0');
+
+      // Format the date as YYYY-MM-DD
+      this.formattedStartDate = `${year}-${month}-${day}`;
+      this.formattedEndDate = `${yearEnd}-${monthEnd}-${dayEnd}`;
+    }
+  }
 
   onBookPlace() {
-    this.modalCtrl.dismiss();
+    this.modalCtrl.dismiss({
+      bookingData: {
+        firstName: this.form.value['first-name'],
+        lastName: this.form.value['last-name'],
+        guestNumber: this.form.value['guest-number'],
+        startDate: this.form.value['date-from'],
+        endDate: this.form.value['date-to'],
+      },
+    });
   }
 
   onCancel() {
     this.modalCtrl.dismiss();
+  }
+
+  datesValid() {
+    const startDate = new Date(this.form.value['date-from']);
+    const endDate = new Date(this.form.value['date-to']);
+    return endDate > startDate;
   }
 }
--- src/app/places/places.service.ts
+++ src/app/places/places.service.ts
@@ -11,21 +11,27 @@ export class PlacesService {
       'Camps Bay Beach',
       'In the heart of Cape town',
       'https://www.capetownwithkids.com/wp-content/uploads/2022/09/Camps-Bay-Beach-Crystal-Water-scaled.jpg',
-      850.99
+      850.99,
+      new Date('2023-01-01'),
+      new Date('2023-02-24')
     ),
     new Place(
       'p2',
       'Sunset Beach',
       'In the outskats of Cape town',
       'https://www.capefear-nc.com/images/uploads/static_content/365/sunset_beach_hero2.jpg',
-      350.99
+      350.99,
+      new Date('2023-02-01'),
+      new Date('2023-03-12')
     ),
     new Place(
       'p3',
       'Cabo Beach Club',
       'Best luxury restaurant of Cape town',
       'https://www.luxuryrestaurantawards.com/wp-content/uploads/sites/9/2022/01/CBC_Pool_6-scaled.jpg',
-      990.99
+      990.99,
+      new Date('2023-03-01'),
+      new Date('2023-04-15')
     )
   ];
 
